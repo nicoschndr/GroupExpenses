4add7e26ed5c2cf16de6b3fe0ae6a2b159f5becd
swhemi <95661259+swhemi@users.noreply.github.com>
2022-07-06 14:34:49 +0200

    adjust styling of ion-segment in expenses.page

--- src/app/expenses/expenses.module.ts
+++ src/app/expenses/expenses.module.ts
@@ -8,7 +8,6 @@ import { ExpensesPageRoutingModule } from './expenses-routing.module';
 
 import { ExpensesPage } from './expenses.page';
 import { AddExpenseComponent } from '../components/add-expense/add-expense.component';
-import { DetailsPageComponent} from '../components/details-page/details-page.component';
 
 @NgModule({
     imports: [
@@ -19,9 +18,9 @@ import { DetailsPageComponent} from '../components/details-page/details-page.com
     ],
     exports: [
         AddExpenseComponent,
-        DetailsPageComponent
     ],
-    declarations: [ExpensesPage, AddExpenseComponent, DetailsPageComponent],
-    entryComponents: [AddExpenseComponent, DetailsPageComponent],
+    declarations: [ExpensesPage, AddExpenseComponent],
+    entryComponents: [AddExpenseComponent],
 })
 export class ExpensesPageModule {}
+
