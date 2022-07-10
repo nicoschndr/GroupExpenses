e5944aa1663e31ce97c36c237d052d456922dfe6
Julia Leimbach <julia.leimbach@mni.thm.de>
2022-07-10 15:14:53 +0200

    doughnut chart implement

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js'; Chart.register(...registerables);
+
 
 @Component({
   selector: 'app-chart',
@@ -7,8 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+  doughnutChart: any;
+
   constructor() { }
 
-  ngOnInit() {}
+  doughnutChartMethod() {
+    this.doughnutChart = new Chart('canvas1', {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
+    });
+  }
+
+  ngOnInit() {
+
+    this.doughnutChartMethod();
+  }
 
 }
